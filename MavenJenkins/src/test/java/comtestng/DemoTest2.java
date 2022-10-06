0beecdb5f3c75e78b0725cc2abb75e1f779731c7
ASUS <ASUS@DESKTOP-QHTOKPE>
2022-10-06 16:32:28 +0530

    adding a new Testing Class

---
+++ MavenJenkins/src/test/java/comtestng/DemoTest2.java
@@ -0,0 +1,15 @@
+package comtestng;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	@Test
+	public void test01() {
+		System.out.println("Hello world");
+	}
+	@Test
+	public void test02() {
+		System.out.println("Hello world");
+
+	}
+}
